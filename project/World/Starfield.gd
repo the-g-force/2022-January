4eb49e8aeb7be6ae576796b18b22c401dfe43e02
the-alex-g <alex.gestwicki@gmail.com>
2022-01-01 11:56:08 -0500

    Randomize colors and sizes in starfield

--- project/World/Starfield.gd
+++ project/World/Starfield.gd
@@ -11,13 +11,38 @@ const STAR := preload("res://World/Star.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	var total_stars := base_stars
 	total_stars += randi()%star_variance
 	for s in total_stars:
-		var star:Spatial = STAR.instance()
+		var star:CSGBox = STAR.instance()
 		var depth := base_depth
 		depth += (randi()%depth_variance*2)-depth_variance
 		var star_x := randi()%screen_size_x
 		var star_y := randi()%screen_size_y
 		star.transform.origin = Vector3(star_x, -depth, star_y)
+		
+		var star_size := 2
+		var star_color_index := randi()%8
+		var star_color := Color.white
+		match star_color_index:
+			0:
+				star_color = Color.aqua
+				star_size += 1
+			1:
+				star_color = Color.yellow
+			2:
+				star_color = Color.orange
+		
+		star_size += randf()-0.3
+		
+		star.width = star_size
+		star.height = star_size
+		star.depth = star_size
+		
+		var star_material := SpatialMaterial.new()
+		star_material.albedo_color = star_color
+		
+		star.material = star_material
+		
 		add_child(star)
