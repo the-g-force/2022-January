bd1e5fb512eb30da3af62d13c7dc96205c469157
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-01-01 13:33:30 -0500

    Placeholder HUD and adjust camera angle and offset

--- project/World/World.gd
+++ project/World/World.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
+export var camera_vertical_offset := 30
+
 func _physics_process(_delta):
 	$Camera.transform.origin.x = $Player.transform.origin.x
-	$Camera.transform.origin.z = $Player.transform.origin.z
+	$Camera.transform.origin.z = $Player.transform.origin.z + camera_vertical_offset
 	
