0d9d65b13c680e4904335902d369fc03ecffbee9
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-01-01 12:17:43 -0500

    Shoot from cannon muzzles

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -9,6 +9,7 @@ var _Laserblast = preload("res://Player/Laserblast.tscn")
 var _can_shoot := true
 
 onready var _shot_timer := $ShotTimer
+onready var _armaments := $Armaments
 
 func _physics_process(_delta):
 	var direction = Vector2(
@@ -26,10 +27,11 @@ func _physics_process(_delta):
 	var _ignored := move_and_collide(Vector3(thrust,0,0).rotated(Vector3.UP, rotation.y))
 
 	if Input.is_action_just_pressed("fire") and _can_shoot:
-		var blast = _Laserblast.instance()
-		get_parent().add_child(blast)
-		blast.transform.origin = transform.origin
-		blast.direction = Vector3(1,0,0).rotated(Vector3.UP, rotation.y)
+		for muzzle in _armaments.get_children():
+			var blast = _Laserblast.instance()
+			get_parent().add_child(blast)
+			blast.transform.origin = muzzle.get_global_transform().origin
+			blast.direction = Vector3(1,0,0).rotated(Vector3.UP, rotation.y)
 		
 		_can_shoot = false
 		_shot_timer.start()
