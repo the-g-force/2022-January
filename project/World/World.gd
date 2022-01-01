75b7e177cda9fd6b619bd2e8590b09e5aa6b039c
the-alex-g <alex.gestwicki@gmail.com>
2022-01-01 14:40:51 -0500

    Add planet where cargo can be dropped off

--- project/World/World.gd
+++ project/World/World.gd
@@ -6,3 +6,8 @@ func _physics_process(_delta):
 	$Camera.transform.origin.x = $Player.transform.origin.x
 	$Camera.transform.origin.z = $Player.transform.origin.z + camera_vertical_offset
 	
+
+
+func _on_Planet_body_entered(body:PhysicsBody)->void:
+	if body.has_method("drop_off"):
+		body.drop_off()
