428d33d37ac5433fe0c8f5797e787814f08a03d4
Jessica Gestwicki <gestwicki@gmail.com>
2022-01-01 15:15:53 -0500

    Add animation to main menu

--- project/Intro/MainMenu.gd
+++ project/Intro/MainMenu.gd
@@ -1,6 +1,17 @@
 extends Control
 
+var pressed : bool = false
+
+func _input(event):	
+	if not pressed and event is InputEventMouseButton:
+		var e = event as InputEventMouseButton
+		if e.is_pressed():
+			# warning-ignore:return_value_discarded
+			get_node("AnimationPlayer").play("splash screen")
+			pressed = true
+
+
 
 func _on_Button_pressed():
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://World/World.tscn")
+ get_tree().change_scene("res://World/World.tscn")
